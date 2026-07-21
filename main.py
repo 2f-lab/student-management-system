0ce331129a5c9c5c92548372d1470b4dfca2b825
2f-lab <briankariuki995@gmail.com>
2026-07-22 00:29:37 +0300

    add OOP remove student with file persistence

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ while True:
     print("\n===== MENU =====")
     print("1. View Students")
     print("2. Add Student")
-    print("3. Exit")
+    print("3. Remove Student")
+    print("4. Exit")
 
     choice = input("Choice: ")
 
@@ -19,11 +20,22 @@ while True:
 
         name = input("Student Name: ")
 
-        age = int(input("Age: "))
+        while True:
+            try:
+                age = int(input("Age: "))
+                break
+            except ValueError:
+                print("Please enter a valid number.")
 
         school.add_student(name, age)
 
     elif choice == "3":
+
+        name = input("Student name to remove: ")
+
+        school.remove_student(name)
+
+    elif choice == "4":
 
         print("Goodbye!")
 
