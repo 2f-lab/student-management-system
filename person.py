4c08ba608410d4ed7a82b396070d4cf258523c44
2f-lab <briankariuki995@gmail.com>
2026-07-24 22:56:25 +0300

    Refactor project using inheritance

---
+++ person.py
@@ -0,0 +1,37 @@
+class Person:
+
+    def __init__(self, name, age):
+
+        self.name = name
+        self.age = age
+
+    @property
+    def age(self):
+
+        return self._age
+
+    @age.setter
+    def age(self, value):
+
+        if value >= 0:
+
+            self._age = value
+
+        else:
+
+            raise ValueError("Age cannot be negative.")
+
+    def introduce(self):
+
+        print(f"My name is {self.name}.")
+        print(f"I am {self.age} years old.")
+
+    def birthday(self):
+
+        self.age += 1
+
+        print(f"Happy Birthday {self.name}!")
+
+    def __str__(self):
+
+        return f"{self.name} ({self.age})"
--- student.py
+++ student.py
@@ -1,39 +1,14 @@
-class Student:
+from person import Person
+
+
+class Student(Person):
 
     def __init__(self, name, age):
 
-        self.name = name
-        self.age = age
-
-    @property
-    def age(self):
-        return self._age
-
-    @age.setter
-    def age(self, value):
-
-        if value >= 0:
-            self._age = value
-        else:
-            raise ValueError("Age cannot be negative.")
-
-    def introduce(self):
-
-        print(f"My name is {self.name}.")
-        print(f"I am {self.age} years old.")
-
-    def birthday(self):
-
-        self.age += 1
-
-        print(f"Happy Birthday {self.name}!")
+        super().__init__(name, age)
 
     def view_details(self):
 
         print("------ STUDENT ------")
         print(f"Name : {self.name}")
         print(f"Age  : {self.age}")
-
-    def __str__(self):
-
-        return f"{self.name} ({self.age})"
