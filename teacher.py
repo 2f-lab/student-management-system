02c1b416efeb73fc282f40fff5318a6b1607f4a8
2f-lab <briankariuki995@gmail.com>
2026-07-25 00:44:26 +0300

    add teacher class and demonstrte polymophism

--- teacher.py
+++ teacher.py
@@ -1,16 +1,16 @@
 from person import Person
 
 
-class Student(Person):
+class Teacher(Person):
 
     def __init__(self, name, age):
         super().__init__(name, age)
 
     def introduce(self):
         super().introduce()
-        print("I am a student.")
+        print("I am a teacher.")
 
     def view_details(self):
-        print("------ STUDENT ------")
+        print("------ TEACHER ------")
         print(f"Name : {self.name}")
         print(f"Age  : {self.age}")
---
+++ test_teacher.py
@@ -0,0 +1,14 @@
+from student import Student
+from teacher import Teacher
+
+
+student = Student("Brian", 25)
+teacher = Teacher("Mr Kim", 45)
+
+print("Student:")
+student.introduce()
+
+print()
+
+print("Teacher:")
+teacher.introduce()
