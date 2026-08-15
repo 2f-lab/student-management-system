f853226fcaca795fa5f0be60dc3dd0c6e2be027a
2f-lab <briankariuki995@gmail.com>
2026-08-15 22:20:26 +0300

    add exception handling for invalid ages

--- person.py
+++ person.py
@@ -4,6 +4,18 @@ class Person:
         self.name = name
         self.age = age
 
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+
+        if value >= 0:
+            self._age = value
+        else:
+            raise ValueError("Age cannot be negative.")
+
     def introduce(self):
         print(f"My name is {self.name}.")
         print(f"I am {self.age} years old.")
